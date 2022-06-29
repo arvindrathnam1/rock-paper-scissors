d26f387c239b00dc13a691878e6819b7a7da87a9
arvindrathnam1 <arvindrathnam1@gmail.com>
2022-06-29 15:44:34 -0400

    basic rps ui

--- rps.js
+++ rps.js
@@ -1,3 +1,40 @@
+let playerScore = 0, 
+    computerScore = 0;
+
+let opt = document.querySelectorAll('.option');
+opt.forEach(op => op.addEventListener('click', result));
+
+function result(e) {
+    let playerSelection = this.id;
+    // console.log(playerSelection);
+    let computerSelection = computerPlay();
+    let res = playRound(playerSelection, computerSelection);
+    let textBox = document.querySelector('.result-text')
+
+    switch(res) {
+        case 0: 
+        textBox.innerText = "Draw!";
+        break;
+        case 1: 
+            textBox.innerText = "You Lose! " + computerSelection + " beats " + playerSelection;
+            computerScore++;
+        break;
+        case 2: 
+            textBox.innerText = "You Win! " + playerSelection + " beats " + computerSelection;
+            playerScore++;
+        break;
+        case 3: 
+            textBox.innerText = "Invalid Choice!";
+            i--;
+        break;
+    }
+
+    let pscore = document.querySelector('.player-score');
+    let cscore = document.querySelector('.computer-score');
+    pscore.innerText = playerScore;
+    cscore.innerText = computerScore;
+}
+
 function computerPlay() {
     const opt = ["rock", "paper", "scissors"];
     const rand = parseInt(Math.random() * 3);
@@ -42,42 +79,40 @@ function playRound(playerSelection, computerSelection) {
     }
 
     return flag;
-
-    
 }
 
-function game() {
-    let playerScore = 0, computerScore = 0;
-    for(let i = 0; i < 5; i++){
-        const computerSelection = computerPlay();
-        const playerSelection = prompt("Choice rock, paper, or scissors", "rock");
-        const res = playRound(playerSelection, computerSelection);
+// function game() {
+//     let playerScore = 0, computerScore = 0;
+//     for(let i = 0; i < 5; i++){
+//         const computerSelection = computerPlay();
+//         const playerSelection = prompt("Choice rock, paper, or scissors", "rock");
+//         const res = playRound(playerSelection, computerSelection);
         
-        switch(res) {
-            case 0: 
-                console.log("Draw!");
-            break;
-            case 1: 
-                console.log("You Lose! " + computerSelection + " beats " + playerSelection);
-                computerScore++;
-            break;
-            case 2: 
-                console.log("You Win! " + playerSelection + " beats " + computerSelection);
-                playerScore++;
-            break;
-            case 3: 
-                console.log("Invalid Choice!");
-                i--;
-            break;
-        }
-    }
+//         switch(res) {
+//             case 0: 
+//                 console.log("Draw!");
+//             break;
+//             case 1: 
+//                 console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+//                 computerScore++;
+//             break;
+//             case 2: 
+//                 console.log("You Win! " + playerSelection + " beats " + computerSelection);
+//                 playerScore++;
+//             break;
+//             case 3: 
+//                 console.log("Invalid Choice!");
+//                 i--;
+//             break;
+//         }
+//     }
 
-    if(computerScore > playerScore)
-        console.log("Computer Wins " + computerScore + " vs " + playerScore);
-    else if(playerScore > computerScore)
-        console.log("Player Wins " + playerScore  + " vs " + computerScore);
-    else
-        console.log("Draw " + computerScore + " vs " + playerScore);
-}
+//     if(computerScore > playerScore)
+//         console.log("Computer Wins " + computerScore + " vs " + playerScore);
+//     else if(playerScore > computerScore)
+//         console.log("Player Wins " + playerScore  + " vs " + computerScore);
+//     else
+//         console.log("Draw " + computerScore + " vs " + playerScore);
+// }
 
-game();
+// game();
